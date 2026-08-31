9596d5317a061520bb9aede3c372011cccce6829
manish202 <godofmind51616@gmail.com>
2026-08-31 19:32:51 +0530

    10 projects added 1st time

--- src/components/projects/constantsAndTypes.ts
+++ src/components/projects/constantsAndTypes.ts
@@ -2,7 +2,7 @@ import React from "react";
 import projects from "./projects";
 
 export const techAndPackages = [
-    
+    'html', 'css', 'javascript', 'jquery', 
 ] as const;
 export type TechAndPackagesOptionsType = typeof techAndPackages[number] | "all";
 
