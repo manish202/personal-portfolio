67fb87b2dace978126aede2b048c5fb637291f8b
manish202 <godofmind51616@gmail.com>
2026-09-01 20:50:11 +0530

    another 10 projects added 8th time

--- src/components/projects/constantsAndTypes.ts
+++ src/components/projects/constantsAndTypes.ts
@@ -6,7 +6,7 @@ export const techAndPackages = [
     'anime.js', 'svg', 'swiper.js', 'mysql', 'react', 'react-dom', 'react-router-dom', 'react-scripts',
     'gh-pages', 'react-toastify', '@emotion/react', '@emotion/styled', '@mui/icons-material',
     '@mui/material', 'firebase', 'react-cookie', 'bcryptjs', 'node', 'express', 'mongodb', 'mongoose',
-    'jsonwebtoken', 'cors', 'dotenv', 
+    'jsonwebtoken', 'cors', 'dotenv', 'wordpress', 
 ] as const;
 export type TechAndPackagesOptionsType = typeof techAndPackages[number] | "all";
 
